19e9c11b4092728ce29bebf4df81cb2fb68d6c81
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-03-19 09:48:44 -0300

    add function to get current weather data

--- src/openWeather.js
+++ src/openWeather.js
@@ -1,12 +1,24 @@
 // Takes in city name, converts name to lat and long for curren weather API
-export default async function getCoords(cityName) {
+export async function getCoords(cityName) {
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=Fredericton&limit=5&appid=862bd6b42e2fcb43a8b4557d161e2601`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=862bd6b42e2fcb43a8b4557d161e2601`
   );
 
   const data = await response.json();
-  const longitude = await data[0].lat;
-  const latitude = await data[0].lon;
+  const longitude = await data[0].lon;
+  const latitude = await data[0].lat;
 
   console.log(longitude, latitude);
+
+  return { latitude, longitude };
+}
+
+export async function getWeatherData(latitude, longitude) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=862bd6b42e2fcb43a8b4557d161e2601`
+  );
+
+  const currentWeather = await response.json();
+
+  console.log(currentWeather);
 }
