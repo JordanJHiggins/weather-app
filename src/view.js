7900ed411c3109561d8947100492a1734916a207
Jordanjhiggins <jordan.higgins14@gmail.com>
2023-03-20 19:34:41 -0300

    add base css and functions to render elements

--- src/view.js
+++ src/view.js
@@ -1,3 +1,14 @@
-function getLocation() {
-  // TODO: Listen to text input, store users selected city.
+function createElement(tag, className) {
+  const element = document.createElement(tag);
+  if (className) element.classList.add(className);
+
+  return element;
+}
+
+export default function renderLocationInput() {
+  const rootElement = document.getElementById('root');
+
+  const inputCard = createElement('div', 'input-card');
+
+  rootElement.appendChild(inputCard);
 }
